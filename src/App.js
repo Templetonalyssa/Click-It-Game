14ef46dccb450974aff8585a56504c4010dd5af6
Alyssa Templeton <templetonalyssa@gmail.com>
2019-01-12 18:40:25 -0500

    added click sum

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import Counter from "./components/Counter";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+    state = {
+      friends,
+      count: 0,
+      clicked: false
+    };
+  
+
+  cardIncrement = id => {
+    // We always use the setState method to update a component's state
+    this.setState({ count: this.state.count + 1 });
+    this.setState({ })
+  };
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const friends = this.state.friends.filter(friend => friend.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ friends });
+  // };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Counter>Score: {this.state.count}/12 </Counter>
+        <Title>Clicky Cats</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            // removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            image={friend.image}
+            cardIncrement={this.cardIncrement}
+            cardTwiceClick={this.cardTwiceClick}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
