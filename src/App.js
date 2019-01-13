d305af6802b86340426e9b7f14eba2c32ebd5b4f
Alyssa Templeton <templetonalyssa@gmail.com>
2019-01-13 15:02:06 -0500

    adding things

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
 import Counter from "./components/Counter";
+import _ from "lodash";
 
 
 class App extends Component {
@@ -44,6 +45,7 @@ class App extends Component {
   };
 
   reset = () => {
+    let 
     console.log("I'm reset")
     this.setState({
       friends: this.state.friends.map((cat) => {
@@ -54,12 +56,7 @@ class App extends Component {
 
     })
   };
-  // removeFriend = id => {
-  //   // Filter this.state.friends for friends with an id not equal to the id being removed
-  //   const friends = this.state.friends.filter(friend => friend.id !== id);
-  //   // Set this.state.friends equal to the new friends array
-  //   this.setState({ friends });
-  // };
+
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
