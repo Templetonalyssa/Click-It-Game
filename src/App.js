f93272c18936db573ddb3f6b256ddd2de235fb85
Alyssa Templeton <templetonalyssa@gmail.com>
2019-01-12 20:21:39 -0500

    reset done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import friends from "./friends.json";
 import Counter from "./components/Counter";
 
+
 class App extends Component {
   // Setting this.state.friends to the friends json array
-    state = {
-      friends,
-      count: 0,
-      clicked: false
-    };
-  
+  state = {
+    friends,
+    count: 0,
+
+  };
+
 
   cardIncrement = id => {
     // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count + 1 });
-    this.setState({ })
+    let clickedCat = false;
+    this.setState({
+      count: this.state.count + 1
+    });
+    this.setState({
+      friends: this.state.friends.map((cat) => {
+        if (cat.id === id) {
+
+          if (cat.clicked === true) {
+            clickedCat = true
+          }
+          cat.clicked = true;
+
+        }
+        return cat;
+
+      })
+    });
+    if (clickedCat === true) {
+      this.reset();
+    }
+
+  };
+
+  reset = () => {
+    console.log("I'm reset")
+    this.setState({
+      friends: this.state.friends.map((cat) => {
+        cat.clicked = false;
+        return cat;
+      }),
+      count: 0
+
+    })
   };
   // removeFriend = id => {
   //   // Filter this.state.friends for friends with an id not equal to the id being removed
@@ -28,23 +63,22 @@ class App extends Component {
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
-    return (
-      <Wrapper>
-        <Counter>Score: {this.state.count}/12 </Counter>
-        <Title>Clicky Cats</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            // removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            image={friend.image}
-            cardIncrement={this.cardIncrement}
-            cardTwiceClick={this.cardTwiceClick}
+    return ( < Wrapper >
+        <Counter > Score: {this.state.count }/12 </Counter >
+        < Title > Clicky Cats </Title> {
+        this.state.friends.map(friend => 
+          ( < FriendCard
+          // removeFriend={this.removeFriend}
+          id = {friend.id}
+          key = {friend.id}
+          image = {friend.image}
+          cardIncrement = {this.cardIncrement}
+          cardTwiceClick = {this.cardTwiceClick}
           />
-        ))}
-      </Wrapper>
-    );
-  }
+        ))
+      } </Wrapper>
+  );
+}
 }
 
 export default App;
