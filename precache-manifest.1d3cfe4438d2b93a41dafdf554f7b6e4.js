47e6d5bcbdced9487eae324248f1a355c14dab72
Alyssa Templeton <templetonalyssa@gmail.com>
2019-01-15 18:13:54 -0500

    Updates

---
+++ precache-manifest.1d3cfe4438d2b93a41dafdf554f7b6e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "8e740076a7536730f444",
+    "url": "/clicky-game/static/js/main.8e740076.chunk.js"
+  },
+  {
+    "revision": "57a303316a909218c372",
+    "url": "/clicky-game/static/js/1.57a30331.chunk.js"
+  },
+  {
+    "revision": "8e740076a7536730f444",
+    "url": "/clicky-game/static/css/main.af620076.chunk.css"
+  },
+  {
+    "revision": "75d984d48ce769b5e8796c72815a8f0d",
+    "url": "/clicky-game/index.html"
+  }
+];
